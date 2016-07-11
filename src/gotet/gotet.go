04b6db0f10a2f352985316fdd4285d5c0b2db24f
Benjamin Lefaudeux <benjamin@theeyetribe.com>
2016-07-11 14:31:14 +0200

    More calls + factoring, missing calibresult & frame

--- src/gotet/gotet.go
+++ src/gotet/gotet.go
@@ -28,7 +28,7 @@ func (c *Client) Close() {
 	c.socket.Close()
 }
 
-// IO helpers
+// Request is an interface{} serving as a JSON skeleleton for all requests
 type Request struct {
 	Category string   `json:"category"`
 	Request  string   `json:"request"`
@@ -36,6 +36,7 @@ type Request struct {
 	// TODO: Implement JSON exports
 }
 
+// Response is an interface{} serving as a JSON skeleleton for all server responses
 type Response struct {
 	Category string                 `json:"category"`
 	Request  string                 `json:"request"`
@@ -66,39 +67,123 @@ func (c *Client) sendReq(cat string, val string) (map[string]interface{}, error)
 	return reply, nil
 }
 
+// readB waits for a given json struct (described by a given field and value) to appear
 func (c *Client) readB(field string, value string) map[string]interface{} {
 	return c.socket.ReadBlock(field, value)
 }
 
+// handleReq is a proxy for the whole "send request and grab server feedback" loop
+func (c *Client) handleReq(cat string, val string) (interface{}, error) {
+	reply, err := c.sendReq(cat, val)
+
+	if err != nil {
+		return "", err
+	}
+	res := reply["values"].(map[string]interface{})
+	
+	return res[val], nil
+}
+
 // Version reports the protocol version currently used
 func (c *Client) Version() (int, error) {
-	reply, err := c.sendReq("tracker", "version")
+	res, err := c.handleReq("tracker", "version")
+
 	if err != nil {
 		return 0, err
 	}
 
-	vers := reply["values"].(map[string]interface{})
-	return int(vers["version"].(float64)), nil
+	return int(res.(float64)), nil
+}
+
+// Trackerstate reports the current tracker state
+// See http://dev.theeyetribe.com/api/#tracker_state for a proper description
+func (c *Client) Trackerstate() (int, error) {
+	res, err := c.handleReq("tracker", "trackerstate")
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(res.(float64)), nil
+}
+
+// Framerate reports the current tracker framerate
+func (c *Client) Framerate() (int, error) {
+	res, err := c.handleReq("tracker", "framerate")
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(res.(float64)), nil
 }
 
 // IsCalibrated reports whether the server is currently calibrated
 func (c *Client) IsCalibrated() (bool, error) {
-	reply, err := c.sendReq("tracker", "iscalibrated")
+	res, err := c.handleReq("tracker", "iscalibrated")
 	if err != nil {
 		return false, err
 	}
 
-	vers := reply["values"].(map[string]interface{})
-	return vers["iscalibrated"].(bool), nil
+	return res.(bool), nil
 }
 
 // IsCalibrating reports whether the server is currently calibrated
 func (c *Client) IsCalibrating() (bool, error) {
-	reply, err := c.sendReq("tracker", "iscalibrating")
+	res, err := c.handleReq("tracker", "iscalibrating")
 	if err != nil {
 		return false, err
 	}
 
-	vers := reply["values"].(map[string]interface{})
-	return vers["iscalibrating"].(bool), nil
+	return res.(bool), nil
+}
+
+// ScreenIndex reports the index of the screen currently in use
+func (c *Client) ScreenIndex() (int, error) {
+	res, err := c.handleReq("tracker", "screenindex")
+	if err != nil {
+		return 0, err
+	}
+
+	return int(res.(float64)), nil
+}
+
+// ScreenResH reports the number of vertical lines on the screen
+func (c *Client) ScreenResH() (int, error) {
+	res, err := c.handleReq("tracker", "screenresh")
+	if err != nil {
+		return 0, err
+	}
+
+	return int(res.(float64)), nil
+}
+
+// ScreenResV reports the number of horizontal lines on the screen
+func (c *Client) ScreenResW() (int, error) {
+	res, err := c.handleReq("tracker", "screenresw")
+	if err != nil {
+		return 0, err
+	}
+
+	return int(res.(float64)), nil
+}
+
+// ScreenPsyW reports the physical (meters) width of the screen
+func (c *Client) ScreenPsyW() (float64, error) {
+	res, err := c.handleReq("tracker", "screenpsyh")
+	if err != nil {
+		return 0, err
+	}
+
+	return res.(float64), nil
+}
+
+// ScreenPsyH reports the physical (meters) height of the screen
+func (c *Client) ScreenPsyH() (float64, error) {
+	res, err := c.handleReq("tracker", "screenpsyh")
+	if err != nil {
+		return 0, err
+	}
+
+	return res.(float64), nil
 }
--- src/test/connect.go
+++ src/test/connect.go
@@ -9,13 +9,34 @@ func main() {
 	client := gotet.Client{}
 	client.Connect("localhost", "6555")
 
-	// Basic API tests
+	// Basic API tests. Check status calls
 	version, _ := client.Version()
 	fmt.Println("Protocol version: ", version)
+
+	trackerstate, _ := client.Trackerstate()
+	fmt.Println("Tracker state: ", trackerstate)
+
+	framerate, _ := client.Framerate()
+	fmt.Println("Framerate: ", framerate)
 
 	iscalibrated, _ := client.IsCalibrated()
 	fmt.Println("Is calibrated ? ", iscalibrated)
 
 	iscalibrating, _ := client.IsCalibrated()
 	fmt.Println("Is calibrating ? ", iscalibrating)
+
+	screenindex, _ := client.ScreenIndex()
+	fmt.Println("Screen index: ", screenindex)
+
+	srh, _ := client.ScreenResH()
+	fmt.Println("Screen horizontal resolution: ", srh)
+
+	srv, _ := client.ScreenResW()
+	fmt.Println("Screen vertical resolution: ", srv)
+
+	spw, _ := client.ScreenPsyW()
+	fmt.Println("Screen physical width: ", spw)
+
+	sph, _ := client.ScreenPsyH()
+	fmt.Println("Screen physical height: ", sph)
 }
